d5abdf87623fe6cfadc407e0db20a83041a9ff9e
Behnaz <behnaz.pourafshari@gmail.com>
2019-07-05 17:40:18 +0100

    html replica

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render () {
+  render() {
     return (
       <div className="app">
         <header className="app__header">
+          <input className="searchInput" />
+          <button className="searchButton">Find Weather</button>
         </header>
         <main className="app__main">
+          <div centerImageDiv>
+            <img
+              className="image"
+              src="https://svs.gsfc.nasa.gov/vis/a010000/a011400/a011482/frames/500x354_7x5_1p/Weather_Icons/01cloudy-t9953_transp.png"
+              alt="cloud"
+            />
+          </div>
         </main>
       </div>
     );

9c3cac0addeb8fbef4ec56c7b268684348f04b15
Behnaz <behnaz.pourafshari@gmail.com>
2019-07-06 22:57:54 +0100

    add netlify

---
+++ build/precache-manifest.53b25febb1312eb02c933f68d3fcbbf6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1496570e5855127d6900ac935cc8ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96d596527ffbb303b62",
+    "url": "/static/css/main.2e2db9ce.chunk.css"
+  },
+  {
+    "revision": "cda009503905214df1d0",
+    "url": "/static/js/2.012c8688.chunk.js"
+  },
+  {
+    "revision": "c96d596527ffbb303b62",
+    "url": "/static/js/main.4f69e93c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0404ecc88c32292dd3defd0e344cca0",
+    "url": "/static/media/clear.a0404ecc.svg"
+  },
+  {
+    "revision": "c2d0aafe5356fbcf85f3c9d95392159b",
+    "url": "/static/media/drizzle.c2d0aafe.svg"
+  },
+  {
+    "revision": "d4db296d45ad7137c9d3d9c298c0193c",
+    "url": "/static/media/fog.d4db296d.svg"
+  },
+  {
+    "revision": "8e5d988eea2cdf0eb2e6ed1b6d6e31a7",
+    "url": "/static/media/mostlyCloudy.8e5d988e.svg"
+  },
+  {
+    "revision": "ed01f1775524d8a7edd64a923cc42053",
+    "url": "/static/media/partlyCloudy.ed01f177.svg"
+  },
+  {
+    "revision": "d6fe62553554d2249b3185873c28be3c",
+    "url": "/static/media/rain.d6fe6255.svg"
+  },
+  {
+    "revision": "041ad5ed49d37fd746212b6ec596c359",
+    "url": "/static/media/snow.041ad5ed.svg"
+  },
+  {
+    "revision": "80c607638524750412e4c4510e0af1b7",
+    "url": "/static/media/storm.80c60763.svg"
+  },
+  {
+    "revision": "06cdd28b1fae35b20c730136ec0c9465",
+    "url": "/static/media/unknown.06cdd28b.svg"
+  }
+]);
